04bd6d7abbff74ec6fed7654b1c948884f537079
Brian Newsom <brian.newsom@colorado.edu>
2016-03-31 22:16:31 -0600

    Finall fix concat

--- concatenate.go
+++ concatenate.go
@@ -1,6 +1,7 @@
 package audiotool
 
 import (
+	"errors"
 	"io/ioutil"
 	"log"
 	"os"
@@ -9,8 +10,8 @@ import (
 	"github.com/briannewsom/audiotool/util"
 )
 
-func ConcatenateToBytes(f1 string, f2 string, ext string) ([]byte, error) {
-	fName, err := Concatenate(f1, f2, ext)
+func ConcatenateToBytes(ext, f1, f2 string) ([]byte, error) {
+	fName, err := Concatenate(ext, f1, f2)
 
 	if err != nil {
 		return nil, err
@@ -23,7 +24,13 @@ func ConcatenateToBytes(f1 string, f2 string, ext string) ([]byte, error) {
 
 	return util.ReadFile(f)
 }
-func Concatenate(f1 string, f2 string, ext string) (string, error) {
+
+func Concatenate(ext string, fs ...string) (string, error) {
+	// TODO: Make this work both directions at the same time
+	if len(fs) < 2 {
+		return "", errors.New("Please provide more than one file to concatenate!")
+	}
+
 	outputFile, err := ioutil.TempFile("/tmp", "concat-")
 
 	if err != nil {
@@ -33,19 +40,32 @@ func Concatenate(f1 string, f2 string, ext string) (string, error) {
 	outputFileName := outputFile.Name() + ext
 	os.Rename(outputFile.Name(), outputFileName)
 
-	log.Printf("Concatenating files %s and %s to %s", f1, f2, outputFileName)
+	log.Printf("Concatenating files to %s", outputFileName)
 
 	if err != nil {
 		return "", err
 	}
 
 	cmd := exec.Command(getAVConvPath(), "-y", "-i",
-		"concat:"+f1+"|"+f2, "-c", "copy", outputFileName)
+		buildConcatCmd(fs...), "-c", "copy", outputFileName)
 
 	err = cmd.Run()
+
 	if err != nil {
 		return "", err
 	}
 
 	return outputFileName, nil
+}
+
+func buildConcatCmd(fs ...string) string {
+	core := ""
+	for i, f := range fs {
+		if i == len(fs)-1 {
+			core += f
+		} else {
+			core += f + "|"
+		}
+	}
+	return "concat:" + core
 }
--- concatenate_test.go
+++ concatenate_test.go
@@ -4,23 +4,32 @@ import (
 	"testing"
 
 	"github.com/briannewsom/audiotool"
-	"github.com/briannewsom/audiotool/util"
+	// "github.com/briannewsom/audiotool/util"
 )
 
 func ExampleConcatenate() {
-	audiotool.Concatenate("_test-res/hi.wav", "_test-res/how-are-you.wav", ".wav")
+	audiotool.Concatenate(".wav", "_test-res/hi.wav", "_test-res/how-are-you.wav")
 }
 
 func ExampleConcatenateToBytes() {
-	audiotool.ConcatenateToBytes("_test-res/hi.wav", "_test-res/how-are-you.wav", ".wav")
+	audiotool.ConcatenateToBytes(".wav", "_test-res/hi.wav", "_test-res/how-are-you.wav")
 }
 
 func TestConcatenate(t *testing.T) {
-	f, err := audiotool.Concatenate("_test-res/hi.wav", "_test-res/how-are-you.wav", ".wav")
+	_, err := audiotool.Concatenate(".wav", "_test-res/hi.wav", "_test-res/how-are-you.wav")
 
 	if err != nil {
 		t.Error("Failed to concatenate audio files - " + err.Error())
 	}
 
-	util.CleanTempFiles(f)
+	/* Concatenate more than 2 files */
+	_, err = audiotool.Concatenate(".wav", "/home/brian/Downloads/be6b9d72-0550-45fa-ae99-157693ac2500-0-t2s.wav",
+		"/home/brian/Downloads/be6b9d72-0550-45fa-ae99-157693ac2500-1-t2s.wav",
+		"/home/brian/Downloads/be6b9d72-0550-45fa-ae99-157693ac2500-2-t2s.wav")
+
+	if err != nil {
+		t.Error("Failed to concatenate four audio files - " + err.Error())
+	}
+
+	// util.CleanTempFiles(f, f2)
 }
